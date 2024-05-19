2cdcc32b29b0014efdd8c14c6ebbd1b4269e6eb1
Assis Ferreira <ferreira.assisruny@gmail.com>
2024-05-19 17:51:07 +0100

    feat: use entity framework and mysql

--- NOS.Engineering.Challenge/Models/Content.cs
+++ NOS.Engineering.Challenge/Models/Content.cs
@@ -1,16 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NOS.Engineering.Challenge.Models;
 
 public class Content
 {
-    public Guid Id { get; }
-    public string Title { get; }
-    public string SubTitle { get; }
-    public string Description { get; }
-    public string ImageUrl { get; }
-    public int Duration { get; }
-    public DateTime StartTime { get; }
-    public DateTime EndTime { get; }
-    public IEnumerable<string> GenreList { get; }
+    [Key]
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string SubTitle { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public int Duration { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public IEnumerable<string> GenreList { get; set; }
+
+    public Content()
+    {
+
+    }
 
 
     public Content(Guid id, string title, string subTitle, string description, string imageUrl, int duration, DateTime startTime, DateTime endTime, IEnumerable<string> genreList)
